ea07a32badf4554b507117ee3d634f39a4d25191
Beretta350 <beretta.gabrielpp@gmail.com>
2023-12-04 00:38:24 -0300

    Validation erros messages

--- src/main/java/com/wex/exchangetransactions/exception/error/ValidationErrorMessages.java
+++ src/main/java/com/wex/exchangetransactions/exception/error/ValidationErrorMessages.java
@@ -11,4 +11,10 @@ public class ValidationErrorMessages {
     public static final String DESCRIPTION_SIZE_MESSAGE = "The description must have size between 0 and 50 characters.";
     public static final String TRANSACTION_DATE_NOT_NULL_MESSAGE = "Transaction date can't be null.";
     public static final String TRANSACTION_TIMESTAMP_NOT_NULL_MESSAGE = "Transaction timestamp can't be null.";
+    public static final String TRANSACTION_ID_NOT_NULL_MESSAGE = "Transaction id can't be null.";
+
+    public static final String RETRIEVE_EXCHANGE_RATE_NOT_NULL_MESSAGE = "Retrieve exchange rate can't be null.";
+    public static final String RETRIEVE_CURRENCY_NOT_NULL_MESSAGE = "Retrieve currency can't be null.";
+    public static final String RETRIEVE_CONVERTED_AMOUNT_ROUNDED_MESSAGE = "Retrieve converted amount is not correctly rounded.";
+    public static final String RETRIEVE_CONVERTED_AMOUNT_NOT_NULL_MESSAGE = "Retrieve converted amount can't be null.";
 }
