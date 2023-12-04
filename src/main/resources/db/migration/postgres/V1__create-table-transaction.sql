a2f6f24a622e8ba487334dc6f002d7cc647ea9d9
Beretta350 <beretta.gabrielpp@gmail.com>
2023-12-04 12:47:07 -0300

    Database fix

--- src/main/resources/db/migration/postgres/V1__create-table-transaction.sql
+++ src/main/resources/db/migration/postgres/V1__create-table-transaction.sql
@@ -7,13 +7,3 @@ CREATE TABLE IF NOT EXISTS "transaction" (
     transaction_date DATE NOT NULL,
     transaction_timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL
 );
-
-CREATE TABLE IF NOT EXISTS "transaction_retrieve_history" (
-    id SERIAL PRIMARY KEY,
-    transaction_id UUID NOT NULL,
-    exchange_rate DOUBLE PRECISION NOT NULL,
-    converted_amount DOUBLE PRECISION NOT NULL,
-    retrieve_timestamp TIMESTAMP,
-    transaction_id_reference UUID REFERENCES "transaction"(id),
-    CONSTRAINT transaction_retrieve_history_transaction_id_key UNIQUE (transaction_id)
-);
