17330a8bb248a1614cb4f563dd21471223c5e7d3
Beretta350 <beretta.gabrielpp@gmail.com>
2023-12-17 20:39:15 -0300

    Fix FOREIGN KEY

--- src/main/resources/db/migration/postgres/V2__create-table-transaction-retrieve-history.sql
+++ src/main/resources/db/migration/postgres/V2__create-table-transaction-retrieve-history.sql
@@ -1,9 +1,9 @@
-CREATE TABLE IF NOT EXISTS transaction_retrieve_history (
+CREATE TABLE IF NOT EXISTS "transaction_retrieve_history" (
     id SERIAL PRIMARY KEY,
     transaction_id UUID NOT NULL,
     currency VARCHAR(30) NOT NULL,
     exchange_rate DOUBLE PRECISION NOT NULL,
     converted_amount DOUBLE PRECISION NOT NULL,
     retrieve_timestamp TIMESTAMP,
-    FOREIGN KEY (transaction_id) REFERENCES transaction(id)
+    FOREIGN KEY (transaction_id) REFERENCES "transaction"(id)
 );
